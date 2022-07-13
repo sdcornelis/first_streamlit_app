fb68afe0a85023e0dccf1b2528a48e08fb69cb9c
Stacy Cornelis <109248867+sdcornelis@users.noreply.github.com>
2022-07-13 17:54:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,3 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bleuberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-
-streamlit.text('Watch me make these changes')
