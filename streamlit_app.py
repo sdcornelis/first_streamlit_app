bdd0ad1de8baafd1ebcd36739d01cc10026da5f4
Stacy Cornelis <109248867+sdcornelis@users.noreply.github.com>
2022-07-14 09:18:51 -0400

    Update streamlit_app.py
    Added default Avocado & Strawberry selection for an example in the build your own fruit smoothie breakfast menu selection list

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index))
+# Let's put a pick list here so they can pick the fruit they want to include (default Avocado & Strawberry selection for an example)
+streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index),['Avocado','Strawberry'])
 
 # Display the table on the page
 streamlit.dataframe(my_fruit_list)
