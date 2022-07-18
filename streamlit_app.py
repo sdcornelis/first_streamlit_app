89f47465e6d5678ab9c47e0ae5779734440efa01
Stacy Cornelis <109248867+sdcornelis@users.noreply.github.com>
2022-07-18 09:06:44 -0400

    Update streamlit_app.py
    Correcting typo in fruityvice call - reequests.get to request.get

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,5 @@ streamlit.dataframe(fruits_to_show)
 
 #New section ti display fruityvice api response
 import requests
-fruityvice_response = reequests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
